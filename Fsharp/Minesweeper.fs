65dd7a59fc50cde4d68b2e07c2dc150f69a98603
Amir Barylko <amir@barylko.com>
2012-03-30 15:13:22 -0500

    First implementation using fsharp

---
+++ Fsharp/Minesweeper.fs
@@ -0,0 +1,25 @@
+﻿module MinesweeperKata.Minesweeper
+
+open System
+
+let parseFieldSize(header:string) =
+    (1, 3)
+
+let fillProximity(rows, cols, mineField) =
+    "*10"
+
+let rec createOutput(lines, outputCount:int) =
+    match lines with
+    | [] -> []
+    | "0 0"::tail -> createOutput(tail, outputCount + 1)
+    | head::tail -> 
+        let (rows, cols) = parseFieldSize head
+        let inputField = tail |> Seq.take(rows) |> Seq.toList
+        let theRest = tail |> Seq.skip(rows) |> Seq.toList
+        String.Format("Field #{0}", outputCount) :: fillProximity(rows, cols, inputField) :: createOutput(theRest, outputCount + 1)
+
+
+let minesweep (fields:string) = createOutput(fields.Split('\n') |> Array.toList, 1)
+    
+
+
---
+++ Fsharp/MinesweeperSpec.fs
@@ -0,0 +1,15 @@
+﻿module MinesweeperKata.MinesweeperSpec
+
+open NaturalSpec
+open Minesweeper
+
+let minesweeping (fields: string) =
+    printMethod ""
+    minesweep(fields)
+
+[<Scenario>]
+let ``Should work with one row``() =
+  Given "1 3\n*.."
+    |> When minesweeping           
+    |> It should equal ["Field #1"; "*10" ]
+    |> Verify                       
