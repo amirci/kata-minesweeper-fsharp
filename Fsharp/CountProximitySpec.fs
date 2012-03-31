9c639b083d0ee117a49f837fb439879ffe1db765
Amir Barylko <amir@barylko.com>
2012-03-30 21:49:56 -0500

    Finished implemented count proximity

--- Fsharp/CountProximitySpec.fs
+++ Fsharp/CountProximitySpec.fs
@@ -5,9 +5,24 @@ open Minesweeper
 open System
 
 [<Scenario>]
-let ``When counting proximity for one row``() =
+let ``When counting proximity for first row``() =
   Given (0, "*..", ["*.."])
     |> When countProximity
     |> It should equal "*10"
     |> Verify
 
+[<Scenario>]
+let ``When counting proximity for second row``() =
+  Given (1, ".*.", ["*.."; ".*."])
+    |> When countProximity
+    |> It should equal "2*1"
+    |> Verify
+
+[<Scenario>]
+let ``When counting proximity for the third row``() =
+  Given (2, "**..", ["*..*"; 
+                     ".*.."; 
+                     "**.."])
+    |> When countProximity
+    |> It should equal "**20"
+    |> Verify
--- Fsharp/Minesweeper.fs
+++ Fsharp/Minesweeper.fs
@@ -9,16 +9,19 @@ let parseFieldSize(header:string) =
     | a::b::_ -> (Int32.Parse a, Int32.Parse b)
         
 
-let countProximity(row, line, mineField) =
+let countProximity(row, line: string, mineField: list<string>) =
+    let explosive c = if c = '*' then 1 else 0
     let neighbours x y = 
-        [1; 2; 3]
+        [(x-1, y-1); (x-1, y); (x-1, y+1); 
+         (x, y-1); (x, y+1); 
+         (x+1, y-1); (x+1, y); (x+1, y+1)]
+            |> List.filter (fun (a , b) -> a >= 0 && b >= 0 && a < mineField.Length && b < line.Length )
+            |> List.map (fun (a, b) -> mineField.Item(a).Chars(b) |> explosive)
+            |> List.sum
 
-    let mapete col c =
-        match c with
-        | '.' -> List.sum(neighbours row col).ToString().Chars(0)
-        | _   -> c
+    let mapete col c = if c = '.' then (neighbours row col).ToString().Chars(0) else c
 
-    line |> String.mapi(mapete)
+    String.mapi mapete line 
 
 let fillProximity(counter:int, rows, cols, mineField) =
     let header = String.Format("Field #{0}", counter) 
@@ -26,20 +29,20 @@ let fillProximity(counter:int, rows, cols, mineField) =
     header :: output |> String.concat "\n"
 
 
-let rec createOutput(counter, lines) =
+let rec createOutput counter lines =
     match lines with
     | [] -> []
-    | "0 0"::tail -> createOutput(counter + 1, tail)
+    | "0 0"::tail -> createOutput (counter + 1) tail
     | head::tail -> 
         let (rows, cols) = parseFieldSize head
         let inputField = tail |> Seq.take(rows) |> Seq.toList
         let theRest = tail |> Seq.skip(rows) |> Seq.toList
-        fillProximity(counter, rows, cols, inputField) :: createOutput(counter + 1, theRest)
+        fillProximity(counter, rows, cols, inputField) :: createOutput (counter + 1) theRest
 
 let minesweep (fields:string) = 
     fields.Split('\n') 
         |> Array.toList
-        |> fun l -> createOutput(1, l)
+        |> fun l -> createOutput 1 l
         |> String.concat "\n"
         
     
